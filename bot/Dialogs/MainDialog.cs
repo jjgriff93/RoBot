1248c61c2640cef0886e83979b6d39b104ceaad4
James Griffin <me@JamesGriff.in>
2023-09-12 09:54:20 -0700

    Add bot code

---
+++ bot/Dialogs/MainDialog.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Robots.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        private readonly RobotRecognizer _cluRecognizer;
+        protected readonly ILogger Logger;
+
+        // Dependency injection uses this constructor to instantiate MainDialog
+        public MainDialog(RobotRecognizer cluRecognizer, ILogger<MainDialog> logger)
+            : base(nameof(MainDialog))
+        {
+            _cluRecognizer = cluRecognizer;
+            Logger = logger;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                IntroStepAsync,
+                ActStepAsync,
+                FinalStepAsync,
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            if (!_cluRecognizer.IsConfigured)
+            {
+                throw new Exception("CLU is not configured correctly. Exiting.");
+            }
+
+            var messageText = stepContext.Options?.ToString() ?? $"What would you like me to do?";
+            var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Call CLU
+            var cluResult = await _cluRecognizer.RecognizeAsync<RobotActions>(stepContext.Context, cancellationToken);
+            switch (cluResult.GetTopIntent().intent)
+            {
+                case RobotActions.Intent.Move:
+                    // Initialize Movement action with any entities we may have found in the response.
+                    var move = new Movement()
+                    {
+                        Object = cluResult.Entities.GetObject(),
+                        Origin = cluResult.Entities.GetOrigin(),
+                        Destination = cluResult.Entities.GetDestination(),
+                    };
+
+                    // TODO: Call Robot API
+
+                    var messageText = $"Moving {move.Object} from {move.Origin} to {move.Destination}";
+                    var message = MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput);
+                    await stepContext.Context.SendActivityAsync(message, cancellationToken);
+                    break;
+
+                default:
+                    // Catch all for unhandled intents
+                    var didntUnderstandMessageText = $"Sorry, did not compute. Please try asking in a different way (intent was {cluResult.GetTopIntent().intent})";
+                    var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
+                    await stepContext.Context.SendActivityAsync(didntUnderstandMessage, cancellationToken);
+                    break;
+            }
+
+            return await stepContext.NextAsync(null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var prompts = new List<string> { "What else can I do?", "Anything else?", "What's your next command?", "I'm ready for my next task." };
+            string promptMessage = prompts[new Random().Next(prompts.Count)];
+            return await stepContext.ReplaceDialogAsync(InitialDialogId, promptMessage, cancellationToken);
+        }
+    }
+}
---
+++ bot/Models/Movement.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Robots
+{
+    public class Movement
+    {
+        public string Object { get; set; }
+
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+    }
+}
